4f2e86af982cb24b6f332863a6f62bb295361339
Park Ju Hyung <qkrwngud825@gmail.com>
2017-01-15 03:33:04 +0900

    SystemUI: Implement burn-in protection for status-bar/nav-bar items
    Devices with OLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    This new feature can be enabled by setting
    config_statusBarBurnInProtection to true.
    
    The shifting interval can be configured with config_shift_interval.
    The default shifting interval is 60 seconds.
    
    Change-Id: I68521a3dd3af941fba0e1fcc0b94d18b1f2f41f9
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -184,6 +184,7 @@ import com.android.systemui.statusbar.policy.BatteryController.BatteryStateChang
 import com.android.systemui.statusbar.policy.BatteryControllerImpl;
 import com.android.systemui.statusbar.policy.BluetoothControllerImpl;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
+import com.android.systemui.statusbar.policy.BurnInProtectionController;
 import com.android.systemui.statusbar.policy.CastControllerImpl;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.FlashlightController;
@@ -342,6 +343,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     FingerprintUnlockController mFingerprintUnlockController;
     LightStatusBarController mLightStatusBarController;
     protected LockscreenWallpaper mLockscreenWallpaper;
+
+    private BurnInProtectionController mBurnInProtectionController;
 
     int mNaturalBarHeight = -1;
 
@@ -857,6 +860,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     R.id.header_debug_info);
             mNotificationPanelDebugText.setVisibility(View.VISIBLE);
         }
+
+        mBurnInProtectionController = new BurnInProtectionController(mContext, mStatusBarView);
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
@@ -5054,6 +5059,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStackScroller.setAnimationsEnabled(false);
         mVisualStabilityManager.setScreenOn(false);
         updateVisibleToUser();
+        if (mBurnInProtectionController != null) {
+            mBurnInProtectionController.stopSwiftTimer();
+        }
         if (mLaunchCameraOnFinishedGoingToSleep) {
             mLaunchCameraOnFinishedGoingToSleep = false;
 
@@ -5074,6 +5082,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mVisualStabilityManager.setScreenOn(true);
         mNotificationPanel.setTouchDisabled(false);
         updateVisibleToUser();
+        if (mBurnInProtectionController != null) {
+            mBurnInProtectionController.startSwiftTimer();
+        }
     }
 
     public void onScreenTurningOn() {
